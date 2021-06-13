77d82fa5df3e41b1a0d37d9ae2a3be7b8076b9e2
alvin <seogi777@gmail.com>
2021-06-13 22:24:25 +0900

    맵 박스 띄우기 시작 코드 추가

--- src/client/components/pages/home/MainHomePage.tsx
+++ src/client/components/pages/home/MainHomePage.tsx
@@ -1,15 +1,29 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 import SEO from '@components/organisms/common/SEO';
 import HomeTemplate from '../../templates/layout/HomeTemplate';
+import { MAPBOX_ACCESS_TOKEN } from '@constants/token';
 
 interface PropsType {}
 
-const MainHomePage: FC<PropsType> = props => {
+const MainHomePage: FC<PropsType> = () => {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
   return (
     <>
       <SEO title="Home" />
       <HomeTemplate>
-        Hello world!
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+          onViewportChange={nextViewport => setViewport(nextViewport)}
+        />
       </HomeTemplate>
     </>
   );
---
+++ src/client/constants/token.ts
@@ -0,0 +1,2 @@
+export const MAPBOX_ACCESS_TOKEN =
+  'pk.eyJ1IjoiYWx2aW5oaGhoIiwiYSI6ImNrcHY1YjdweTA1NHUybm1ydXdsb2hibjMifQ.9vc8iirhkj6miPdK_-9Cjw';
